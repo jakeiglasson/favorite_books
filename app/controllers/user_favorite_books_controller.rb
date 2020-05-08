dfe87b4d6db02e8a5e7cf8d69b52ff660af2024c
Jake Glasson <jakeiglasson@gmail.com>
2020-05-08 14:11:09 +1000

    fixed redirect issue in UserFavoriteBooksController

--- app/controllers/user_favorite_books_controller.rb
+++ app/controllers/user_favorite_books_controller.rb
@@ -17,7 +17,7 @@ class UserFavoriteBooksController < ApplicationController
       user_id: current_user.id
     )
     # render json: { status: :true }
-    redirect_to pages_path
+    redirect_to root_path
   end
 
 end
