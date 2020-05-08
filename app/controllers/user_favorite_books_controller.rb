0d394410045a45bdbe6fbc0c939bd73413477466
DannyField <dannyfieldproductions@gmail.com>
2020-05-08 14:45:43 +1000

    Add button working

--- app/controllers/user_favorite_books_controller.rb
+++ app/controllers/user_favorite_books_controller.rb
@@ -17,7 +17,7 @@ class UserFavoriteBooksController < ApplicationController
       user_id: current_user.id
     )
     # render json: { status: :true }
-     redirect_to pages_path
+    redirect_to root_path
     # redirect_to books_path
     # redirect_to user_favorite_books_path
   end
